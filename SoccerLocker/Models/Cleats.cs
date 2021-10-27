f5a0e96db6836e9c190a5195916b9e674127beab
Adrian Garcia Rios <agarciar@GMU.EDU>
2021-10-27 19:42:28 -0400

    Added Data Annotations to Cleats class

--- SoccerLocker/Models/Cleats.cs
+++ SoccerLocker/Models/Cleats.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,26 @@ namespace SoccerLocker.Models
 {
     public class Cleats
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [Display(Name="Brand")]
+        [StringLength(50)]
         public string cleatBrand { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        [StringLength(50)]
         public string cleatName { get; set; }
+        [Required]
+        [Display(Name = "Size(US)")]
+        [StringLength(50)]
         public string size { get; set; }
+        [Required]
+        [Display(Name = "Price($)")]
         public decimal price { get; set; }
+        [Required]
+        [Display(Name = "Color")]
+        [StringLength(50)]
         public string color { get; set; }
     }
 }
