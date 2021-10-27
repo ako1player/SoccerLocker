1702d455360c744501de6effd22532a5e7890f99
Adrian Garcia Rios <agarciar@GMU.EDU>
2021-10-27 12:08:44 -0400

    Added cleats model class

---
+++ SoccerLocker/Models/Cleats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoccerLocker.Models
+{
+    public class Cleats
+    {
+        public int Id { get; set; }
+        public string cleatBrand { get; set; }
+        public string cleatName { get; set; }
+        public string size { get; set; }
+        public decimal price { get; set; }
+        public string color { get; set; }
+    }
+}
